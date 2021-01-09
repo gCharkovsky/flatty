bc701d251dfc2634a08c4e69c5250ecd397686c8
gcharkovskiy <g.charkovsky@gmail.com>
2021-01-09 18:30:06 +0300

    Initial commit

---
+++ field.py
@@ -0,0 +1,15 @@
+class Point(object):
+    value = ""
+    ways = [0, 0, 0, 0]
+
+
+class Field(object):
+    points = [
+        {
+            "value": "",
+            "ways": [0, 0, 0, 0]
+        }
+    ]
+
+    def __init__(self, seed, config):
+        pass
---
+++ players/abstractPlayer.py
@@ -0,0 +1,6 @@
+class AbstractPlayer(object):
+    def make_move(self):
+        return 0
+
+    def parse_result(self):
+        return 1
